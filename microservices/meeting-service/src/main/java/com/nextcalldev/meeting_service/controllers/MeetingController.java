3bacdeeb14bbe69b88943d21f9630289559c96f8
Markus <abramianmedina@example.com>
2025-03-17 21:36:40 +0000

    Cors configured

--- microservices/meeting-service/src/main/java/com/nextcalldev/meeting_service/controllers/MeetingController.java
+++ microservices/meeting-service/src/main/java/com/nextcalldev/meeting_service/controllers/MeetingController.java
@@ -1,6 +1,7 @@
 package com.nextcalldev.meeting_service.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import com.nextcalldev.meeting_service.models.dto.CreateMeetingDto;
 import com.nextcalldev.meeting_service.services.IMeetingService;
 import com.nextcalldev.meeting_service.services.MeetingServiceImpl;
 
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController()
 @RequestMapping("/meeting")
 public class MeetingController {
